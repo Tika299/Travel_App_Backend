012f8f626e92415694a8a3862068a285a62debe1
Tika299 <141425794+Tika299@users.noreply.github.com>
2025-11-08 14:42:12 +0700

    Update CheckinPlaceController.php

--- app/Http/Controllers/Api/CheckinPlaceController.php
+++ app/Http/Controllers/Api/CheckinPlaceController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\CheckinPlace;
+use App\Models\CheckInPlace;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
